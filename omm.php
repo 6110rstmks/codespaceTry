804feabd140c5f51ab31cec1a4d2616fd761ab89
6110rstmks <92964241+6110rstmks@users.noreply.github.com>
2022-12-30 03:15:15 +0000

    uiuiuiu

--- omm.php
+++ omm.php
@@ -1,2 +1,29 @@
 <div>99</div>
-<div><?= 34 ?></div>
+<div><?= 'あ' ?></div>
+
+<?php echo 3 + 3;
+
+
+function mysqlConnect()
+{
+    // $dbhost = 'localhost';
+    // $dbuser = "ss119";
+    // $dbpass = "119089";
+    // $db = "laravel";
+
+    // try {
+    //     $conn = new PDO("mysql:host=$dbhost;db=$db;", $dbuser, $dbpass);
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo "connected suceess";
+    // } catch(PDOExeption $e)
+    // {
+    //     echo $e->getMessage();
+    // }
+    echo 'a' + 'b';
+
+}
+
+mysqlConnect();
+
+?>
+
